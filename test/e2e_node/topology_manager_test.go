98da4a34aee9222b10d45d13653b1a080b4d335b
Francesco Romani <fromani@redhat.com>
2020-11-03 13:30:25 +0100

    node: e2e: wait for all sriovdp containers to be gone
    It's a little but safer and cleaner to wait for all
    the SRIOV dp containers to be gone, not just for the first one.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e_node/topology_manager_test.go
+++ test/e2e_node/topology_manager_test.go
@@ -514,7 +514,7 @@ func teardownSRIOVConfigOrFail(f *framework.Framework, sd *sriovData) {
 	ginkgo.By(fmt.Sprintf("Delete SRIOV device plugin pod %s/%s", sd.pod.Namespace, sd.pod.Name))
 	err = f.ClientSet.CoreV1().Pods(sd.pod.Namespace).Delete(context.TODO(), sd.pod.Name, deleteOptions)
 	framework.ExpectNoError(err)
-	waitForContainerRemoval(sd.pod.Spec.Containers[0].Name, sd.pod.Name, sd.pod.Namespace)
+	waitForAllContainerRemoval(sd.pod.Name, sd.pod.Namespace)
 
 	ginkgo.By(fmt.Sprintf("Deleting configMap %v/%v", metav1.NamespaceSystem, sd.configMap.Name))
 	err = f.ClientSet.CoreV1().ConfigMaps(metav1.NamespaceSystem).Delete(context.TODO(), sd.configMap.Name, deleteOptions)
