3a40ab440a09655d55bde1579e05956fa79afd68
Francesco Romani <fromani@redhat.com>
2021-04-09 11:56:05 +0200

    WIP: smtware: allocate only full cores
    very first draft, possibly broken.
    Intentionally duplicates code.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/kubelet/cm/cpumanager/policy_smtaware.go
+++ pkg/kubelet/cm/cpumanager/policy_smtaware.go
@@ -20,9 +20,12 @@ import (
 	"fmt"
 
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/pkg/kubelet/cm/cpumanager/state"
 	"k8s.io/kubernetes/pkg/kubelet/cm/cpumanager/topology"
 	"k8s.io/kubernetes/pkg/kubelet/cm/cpuset"
 	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager"
+	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager/bitmask"
 	"k8s.io/kubernetes/pkg/kubelet/lifecycle"
 )
 
@@ -53,11 +56,126 @@ func (p *smtAwarePolicy) Name() string {
 }
 
 func (p *smtAwarePolicy) Admit(pod *v1.Pod) lifecycle.PodAdmitResult {
+	cpusPerCore := p.topology.CPUsPerCore()
 	for _, container := range append(pod.Spec.InitContainers, pod.Spec.Containers...) {
-		// TODO: 2 or threads_per_core?
-		if requested := p.guaranteedCPUs(pod, &container); requested > 0 && requested%2 != 0 {
+		if requested := p.guaranteedCPUs(pod, &container); requested > 0 && (requested%cpusPerCore) != 0 {
+			// we intentionally don't check we have enough free cores. We just check we have enough free cpus.
+			// This works because the allocation later on will be performed in terms of full cores (or multiples of).
+			// TODO: expand.
 			return coresAllocationError()
 		}
 	}
 	return p.staticPolicy.Admit(pod)
+}
+
+func (p *smtAwarePolicy) Allocate(s state.State, pod *v1.Pod, container *v1.Container) error {
+	if numCPUs := p.guaranteedCPUs(pod, container); numCPUs != 0 {
+		klog.InfoS("SMTAware policy: Allocate", "pod", klog.KObj(pod), "containerName", container.Name)
+		// container belongs in an exclusively allocated pool
+
+		if cpuset, ok := s.GetCPUSet(string(pod.UID), container.Name); ok {
+			p.updateCPUsToReuse(pod, container, cpuset)
+			klog.InfoS("SMTAware policy: container already present in state, skipping", "pod", klog.KObj(pod), "containerName", container.Name)
+			return nil
+		}
+
+		// Call Topology Manager to get the aligned socket affinity across all hint providers.
+		hint := p.affinity.GetAffinity(string(pod.UID), container.Name)
+		klog.InfoS("Topology Affinity", "pod", klog.KObj(pod), "containerName", container.Name, "affinity", hint)
+
+		// Allocate CPUs according to the NUMA affinity contained in the hint.
+		cpuset, err := p.allocateCPUs(s, numCPUs, hint.NUMANodeAffinity, p.cpusToReuse[string(pod.UID)])
+		if err != nil {
+			klog.ErrorS(err, "Unable to allocate CPUs", "pod", klog.KObj(pod), "containerName", container.Name, "numCPUs", numCPUs)
+			return err
+		}
+		s.SetCPUSet(string(pod.UID), container.Name, cpuset)
+		p.updateCPUsToReuse(pod, container, cpuset)
+
+	}
+	// container belongs in the shared pool (nothing to do; use default cpuset)
+	return nil
+}
+
+func (p *smtAwarePolicy) allocateCPUs(s state.State, numCPUs int, numaAffinity bitmask.BitMask, reusableCPUs cpuset.CPUSet) (cpuset.CPUSet, error) {
+	klog.InfoS("AllocateCPUs", "numCPUs", numCPUs, "socket", numaAffinity)
+
+	allocatableCPUs := p.GetAllocatableCPUs(s).Union(reusableCPUs)
+
+	// If there are aligned CPUs in numaAffinity, attempt to take those first.
+	result := cpuset.NewCPUSet()
+	if numaAffinity != nil {
+		alignedCPUs := cpuset.NewCPUSet()
+		for _, numaNodeID := range numaAffinity.GetBits() {
+			alignedCPUs = alignedCPUs.Union(allocatableCPUs.Intersection(p.topology.CPUDetails.CPUsInNUMANodes(numaNodeID)))
+		}
+
+		numAlignedToAlloc := alignedCPUs.Size()
+		if numCPUs < numAlignedToAlloc {
+			numAlignedToAlloc = numCPUs
+		}
+
+		alignedCPUs, err := takeCoresByTopology(p.topology, alignedCPUs, numAlignedToAlloc)
+		if err != nil {
+			return cpuset.NewCPUSet(), err
+		}
+
+		result = result.Union(alignedCPUs)
+	}
+
+	// Get any remaining CPUs from what's leftover after attempting to grab aligned ones.
+	remainingCPUs, err := takeCoresByTopology(p.topology, allocatableCPUs.Difference(result), numCPUs-result.Size())
+	if err != nil {
+		return cpuset.NewCPUSet(), err
+	}
+	result = result.Union(remainingCPUs)
+
+	// Remove allocated CPUs from the shared CPUSet.
+	s.SetDefaultCPUSet(s.GetDefaultCPUSet().Difference(result))
+
+	klog.InfoS("AllocateCPUs", "result", result)
+	return result, nil
+}
+
+func takeCoresByTopology(topo *topology.CPUTopology, availableCPUs cpuset.CPUSet, numCPUs int) (cpuset.CPUSet, error) {
+	acc := newCPUAccumulator(topo, availableCPUs, numCPUs)
+	if acc.isSatisfied() {
+		return acc.result, nil
+	}
+	if acc.isFailed() {
+		return cpuset.NewCPUSet(), fmt.Errorf("not enough cpus available to satisfy request")
+	}
+
+	// Algorithm: topology-aware best-fit
+	// 1. Acquire whole sockets, if available and the container requires at
+	//    least a socket's-worth of CPUs.
+	if acc.needs(acc.topo.CPUsPerSocket()) {
+		for _, s := range acc.freeSockets() {
+			klog.V(4).InfoS("takeCoresByTopology: claiming socket", "socket", s)
+			acc.take(acc.details.CPUsInSockets(s))
+			if acc.isSatisfied() {
+				return acc.result, nil
+			}
+			if !acc.needs(acc.topo.CPUsPerSocket()) {
+				break
+			}
+		}
+	}
+
+	// 2. Acquire whole cores, if available and the container requires at least
+	//    a core's-worth of CPUs.
+	if acc.needs(acc.topo.CPUsPerCore()) {
+		for _, c := range acc.freeCores() {
+			klog.V(4).InfoS("takeCoresByTopology: claiming core", "core", c)
+			acc.take(acc.details.CPUsInCores(c))
+			if acc.isSatisfied() {
+				return acc.result, nil
+			}
+			if !acc.needs(acc.topo.CPUsPerCore()) {
+				break
+			}
+		}
+	}
+
+	return cpuset.NewCPUSet(), fmt.Errorf("failed to allocate cpus")
 }
